41d715df11caef8fac9f290cce24fadf4f191f8f
James Rapley <rapleyj@gmail.com>
2013-12-02 21:26:42 -0600

    update email validation and tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -42,7 +42,7 @@ describe User do
 
   describe "when email format is invalid" do
   	it "should be invalid" do
-  	  addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar+baz.com]
+  	  addresses = %w[user@foo,com user_at_foo.org example.user@foo. foo@bar_baz.com foo@bar..com foo@bar+baz.com]
   	  addresses.each do |invalid_address|
   	  	@user.email = invalid_address
   	  	expect(@user).not_to be_valid
@@ -98,5 +98,15 @@ describe User do
   describe "with a password that is too short" do
   	before { @user.password = @user.password_confirmation = "a" * 5 }
   	it { should_not be_valid }
+  end
+
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be downcast on save" do
+      @user.email = mixed_case_email;
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
   end
 end
